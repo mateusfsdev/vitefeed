b356c7c1d661daad3c1dedf6b72317dfb0fd180a
mateus sant'ana <mateusfdev@outlook.com>
2024-02-02 09:23:43 -0300

    timer styled not font 🀄🀄

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,39 +1,37 @@
 
-import Calculator from '../projects/Calc';
 import { Discord } from '../projects/Discord';
 import { Fanfuria } from '../projects/Fanfuria';
-import Imccalc from '../projects/Imccalc';
 import { Pokedex } from '../projects/Pokedex';
 import { Timer } from '../projects/Timer';
 import { Tweed } from '../projects/Tweed';
+
+import Calculator from '../projects/Calc';
+import Imccalc from '../projects/Imccalc';
 import styles from './Projects.module.css'
 
-// import { Container } from './styles';
 export default function Projects() {
 return (
-
-<>
-<div className={styles.wrappergrid}>
-    <Calculator/>
-    <Imccalc/>
-  </div>
-  <div className={styles.wrapper}>
-    <Timer />
-  </div>
-  <div className={styles.wrapper}>
-    <Pokedex/>
-  </div>
-  <div className={styles.wrapper}>
-    <Discord/>
-  </div>
-  <div className={styles.wrapper}>
-    <Fanfuria />
-  </div>
-  <div className={styles.wrapper}>
-    <Tweed/>
-  </div>
-
-
-</>
+  <>
+    <div className={styles.wrappergrid}>
+      <Calculator/>
+      <Imccalc/>
+    </div>
+    <div className={styles.wrapper}>
+      <Timer />
+    </div>
+    <div className={styles.wrapper}>
+      <Pokedex/>
+    </div>
+    <div className={styles.wrapper}>
+      <Discord/>
+    </div>
+    <div className={styles.wrapper}>
+      <Fanfuria />
+      
+    </div>
+    <div className={styles.wrapper}>
+      <Tweed/>
+    </div>
+  </>
 )
 }
--- src/projects/Timer.tsx
+++ src/projects/Timer.tsx
@@ -1,11 +1,17 @@
 import styles from './Timer.module.css'
 
-
 export function Timer() {
   return (
-  <div className={styles.wrapper}>
+    <div className={styles.wrapper}>
     <div className={styles.container}>
       <h1>Timer</h1>
+    </div>
+    <div className={styles.countdown}>
+      <span>0</span>
+      <span>1</span>
+      <div>:</div>
+      <span>0</span>
+      <span>0</span>
     </div>
 
   </div>
